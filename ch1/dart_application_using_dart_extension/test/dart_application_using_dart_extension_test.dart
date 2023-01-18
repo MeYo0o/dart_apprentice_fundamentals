6566e0ff42a2dd94ead6e7193c1b97ceb1deb467
Moaz Ahmed <justmeyo2025@gmail.com>
2023-01-18 06:45:07 +0200

    ch1

---
+++ ch1/dart_application_using_dart_extension/test/dart_application_using_dart_extension_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_using_dart_extension/dart_application_using_dart_extension.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
---
+++ ch1/hello_project/test/hello_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:hello_project/hello_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
